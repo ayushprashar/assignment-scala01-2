6b26c0526b2f6c98fc390b36fbb3f5ddd71e2036
Ayush Prashar <ayush.prashar@knoldus.in>
2018-01-09 17:42:33 +0530

    removed magicnumber warning

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,7 +1,16 @@
 object MainApp extends App{
   val exec = new Operations
-  val toPrintIndex = List(10, 20, 30, 40)
-  val toFindMax = List(1,5,2,9,7)
+  val toPrintIndex1 = 10
+  val toPrintIndex2 = 20
+  val toPrintIndex3 = 30
+  val toPrintIndex4 = 40
+  val toPrintIndex = List(toPrintIndex1,toPrintIndex2,toPrintIndex3,toPrintIndex4)
+  val toFindMax1 = 1
+  val toFindMax2 = 5
+  val toFindMax3 = 2
+  val toFindMax4 = 9
+  val toFindMax5 = 7
+  val toFindMax = List(toFindMax1,toFindMax2,toFindMax3,toFindMax4,toFindMax5)
   val fibonacciPlaceholder = 4
   val sopod_placeholder = 5
   exec.print_indices(toPrintIndex)
